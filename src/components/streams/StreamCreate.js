cdb4c12d8f18c6f46ea46a52ed9441178c47adf7
AV <vihrogonov@protonmail.com>
2021-06-11 15:13:57 +0100

    Create Streams Components

---
+++ src/components/streams/StreamCreate.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+const StreamCreate = () => {
+  return (
+    <div>StreamCreate</div>
+  );
+}
+
+export default StreamCreate;
+
---
+++ src/components/streams/StreamEdit.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const SreamEdit = () => {
+  return <div>SreamEdit</div>;
+};
+
+export default SreamEdit;
